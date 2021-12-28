ae06009d4bb5c4d315f73da68772a3f1227a8205
cjd1985 <domemacintosh@gmail.com>
2021-12-28 14:17:36 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-ng-gh-netlify';
+
+  custom_env = process.env.NG_APP_ENV
+
+  ngOnInit() {
+    console.log('---env---', this.custom_env);
+    console.warn(environment)
+
+  }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_id: process.env.NG_APP_ENV
 };
