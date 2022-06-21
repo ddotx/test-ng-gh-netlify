2541c4f38df32e5d3e556015cee5ac14cf19949b
ddotx <domemacintosh@gmail.com>
2022-06-21 17:00:32 +0700

    update netlify function

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
-  console.log('---trigger event---',event)
-  console.log('---trigger context---',context)
+  console.log('---trigger event (dev)---',event)
+  console.log('---trigger context (dev)---',context)
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "deploy succeeded from netlify" }),
